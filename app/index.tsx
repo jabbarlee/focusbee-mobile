95d20b2fff779fc09114e8c7a4174642f06e4d56
jabbarlee <amiljabbarlee@gmail.com>
2025-11-18 20:26:42 -0600

    Refactor project layout by removing unused components and simplifying the RootLayout structure, while updating configuration files for improved integration with Nativewind and Metro bundler.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "@/styles/global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-xl font-bold text-white">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
